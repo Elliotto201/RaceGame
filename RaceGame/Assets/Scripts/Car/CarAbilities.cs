3c02e8d2d95d65369e897dd7c2519246587368eb
Elliotto201 <elliotto.furmanski@gmail.com>
2025-09-28 23:46:56 +0200

    Damage Added, Issue of health UI acting weird

--- RaceGame/Assets/Scripts/Car/CarAbilities.cs
+++ RaceGame/Assets/Scripts/Car/CarAbilities.cs
@@ -10,7 +10,23 @@ public class CarAbilities : NetworkBehaviour
 
     public void SelfExplode()
     {
+        var colliders = Physics.OverlapSphere(transform.position, 8, LayerMask.GetMask("Player"));
+        var selfCar = GetComponent<Car>();
 
+        foreach(var collider in colliders)
+        {
+            if(collider.TryGetComponent(out Car car))
+            {
+                if(selfCar != car)
+                {
+                    car.Damage(int.MaxValue - 1);
+                }
+                else
+                {
+                    car.Damage(selfCar.CurrentHealth - 1);
+                }
+            }
+        }
 
         SpawnExplosionEffectRpc();
     }
